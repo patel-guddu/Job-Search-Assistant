6a66745b65e31a3c7cab83ceac81de162ffaba16
patel-guddu <patelguddu030@gmail.com>
2024-04-21 17:00:39 +0530

    first commit

---
+++ Contact.php
@@ -0,0 +1,43 @@
+<section id="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="about-logo">
+					<h3>Voluptatem Accusantium Doloremque</h3>
+					<p>Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas</p>
+					<p>Sed ut perspiciaatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do tempor.</p>
+				<!-- Form itself -->
+				<form name="sentMessage" id="contactForm" novalidate>
+					<h3>Contact me</h3>
+					<div class="control-group">
+						<div class="controls">
+							<input type="text" class="form-control" placeholder="Full Name" id="name" required data-validation-required-message="Please enter your name" />
+							<p class="help-block"></p>
+						</div>
+					</div>
+					<div class="control-group">
+						<div class="controls">
+							<input type="email" class="form-control" placeholder="Email" id="email" required data-validation-required-message="Please enter your email" />
+						</div>
+					</div>
+					<div class="control-group">
+						<div class="controls">
+							<textarea rows="10" cols="100" class="form-control" placeholder="Message" id="message" required data-validation-required-message="Please enter your message" minlength="5" data-validation-minlength-message="Min 5 characters" maxlength="999" style="resize:none"></textarea>
+						</div>
+					</div>
+					<div id="success"></div> <!-- For success/fail messages -->
+					<button type="submit" class="btn btn-primary pull-right">Send</button><br />
+				</form>
+			</div>
+			<div class="col-md-6">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3556.978714579502!2d80.93119097444199!3d26.935889259095983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39995751ab44fe3b%3A0x38a1755605a18394!2sBansal%20institute%20of%20engineering%20and%20technology%20lucknow!5e0!3m2!1sen!2sin!4v1713122281297!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+			</div>
+		</div>
+	</div>
+</section>
